aeb36f84482e3e024abee8330690a72652764dd5
shubham <shubhamtailor336@gmail.com>
2026-06-22 21:31:34 +0530

    add new file , control_flow.js

---
+++ control_flow.js
@@ -0,0 +1,132 @@
+let age = 20;
+
+if(age<=12&&age<=20){
+    console.log("eligible")}
+else{
+    console.log("not eligible")
+};
+
+let num =10;
+
+if(num%2===0){
+console.log("even number")
+}else{
+    console.log("odd number")
+}
+
+let age1 = 18;
+
+if(age1>=18){
+    console.log(
+        "you can vote"
+    )
+}else{
+    console.log(
+        "you cannot vote")
+    
+}
+
+let number = -5;
+
+
+if(number>0){
+    console.log("positive")
+}
+else if (number<0){
+    console.log("nagatve")
+}
+else
+{
+    console.log("zero")
+}
+
+let a = 10 ;
+let b = 20;
+let c = 30;
+
+if (( a>b || a>b) && c>b){
+console.log("right")
+}
+else{
+    console.log("wrong")
+}
+
+let age3 = 30;
+
+if(age3>=18){
+    console.log("Adult");
+
+}else
+{
+    console.log("minor")
+}
+
+let marks = 32;
+
+if(marks>=33){
+    console.log("pass")
+}
+else{
+    console.log("fail")
+}
+
+let temp = 29;
+
+if(temp>=30){
+    console.log(
+        "Hot"
+    )
+}
+else if (temp<30){
+    console.log(
+        "Cool"
+    )
+}
+else{
+    "Cool"
+}
+
+let x = 10;
+let y = 20;
+let z = 30;
+
+if(x>y){
+    console.log("true")
+}
+else if (y>z){
+console.log("right")
+}
+
+else if(z>y){
+    console.log("yes")
+}
+else{
+    console.log("Not find the value")
+}
+let a1 = 10;
+let b1 = 25;
+let c1 = 15;
+
+if (a1 > b1 && a1 > c1) {
+    console.log("A is greatest:", a1);
+} 
+else if (b1 > a1&& b1 > c1) {
+    console.log("B is greatest:", b1);
+} 
+else {
+    console.log("C is greatest:", c1);
+}
+
+let year = 2026;
+
+if(year%400===0){
+console.log("leap year")
+}
+else if(year%4 && year%100!==0){
+    console.log(
+        "Leap year"
+    )
+}
+else{
+console.log("Not leap year")
+}
